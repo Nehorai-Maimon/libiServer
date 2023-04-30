ba326520436a0ce4ddfb0bd32ecdcf01d72c4818
נהוראי <nehoraimimon@gmail.com>
2023-04-30 09:45:35 +0300

    .

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const express = require("express"),
     cors = require("cors"),
     studentRoute = require("./student/studentRoute"),
     workerRoute = require("./worker/workerRoute"),
-    workerModel = require("./worker/workerModel"),
     eventRoute = require("./event/eventRoute");
 
 app.use(cors())
 app.use(express.json())
+app.use((req,res,next)=>{
+
+    console.log(req.ip);
+    next();
+})
 app.use("/student", studentRoute)
 app.use("/event", eventRoute)
 app.use("/worker", workerRoute)
 
-app.get("/", (req,res) => res.send("hay!!!"))
+app.get("/", (req, res) => res.send("hay!!!"))
 
 app.listen(PORT, () => console.log(`server listening on ${PORT}`))
 require("./db").connect()
